a5d5be336c1f47f6e86732ba2da8e27752d86700
Mohatshim Khan <mohatshim_khan@yahoo.co.in>
2019-05-26 22:25:42 +0530

    Changes in url model file

--- application/models/Url_model.php
+++ application/models/Url_model.php
@@ -1,95 +1,96 @@
 <?php
 	class Url_Model extends CI_Model {
 
-	public function __construct(){
+		public function __construct(){
 
-		parent::__construct();
-	}	
+			parent::__construct();
+		}	
 
-	/**
-     * Check records for already existing url 
-     * @param $url
-     * @return Bool true or false
-     */
-	public function check_url_exists($url){
+		/**
+	     * Check records for already existing url 
+	     * @param $url
+	     * @return Bool true or false
+	     */
+		public function check_url_exists($url){
 
-		$this->db->where('url', $url);
-		
-		$query = $this->db->get('urls');
-		
-		$num_rows = $query->num_rows();
-
-		if($num_rows>0){
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	/**
-     * Save new record to the table
-     * @param $data
-     * @return Bool true or false
-     */
-	public function create_short($data){
-
-		$insert = $this->db->insert('urls', $data);
-
-		if($insert){
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	/**
-     * Retrieve the number of existing records.
-     * @return integer
-     */
-	public function get_url_num_rows(){
-		
-				 $this->db->order_by('id', 'DESC');	
-		$query = $this->db->get('urls');
-		
-		$num_rows = $query->num_rows();
-
-		return $num_rows;
-	}
-
-	/**
-     * Retrieve top recent 100 url records. 
-     * @return array
-     */
-	public function get_top_url(){
-				  $this->db->order_by('id','DESC');
-		$result = $this->db->get('urls', 0, 100)->result_array();
-		return $result;
-	} 
-
-	/**
-     * Check records for already existing short 
-     * @param $short
-     * @return array or false
-     */
-	public function check_short_exists($short){
-		
-				 $this->db->where('short', $short);
-		$query = $this->db->get('urls');
-
-		$num_rows = $query->num_rows();
-
-		if($num_rows>0){
+			$this->db->where('url', $url);
 			
-			$result = $query->row_array();
-		
-			return $result;
+			$query = $this->db->get('urls');
+			
+			$num_rows = $query->num_rows();
 
-		} else {
-
-			return false;
+			if($num_rows>0){
+				return true;
+			} else {
+				return false;
+			}
 		}
+
+		/**
+	     * Save new record to the table
+	     * @param $data
+	     * @return Bool true or false
+	     */
+		public function create_short($data){
+
+			$insert = $this->db->insert('urls', $data);
+
+			if($insert){
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		/**
+	     * Retrieve the number of existing records.
+	     * @return integer
+	     */
+		public function get_url_num_rows(){
+			
+					 $this->db->order_by('id', 'DESC');	
+			$query = $this->db->get('urls');
+			
+			$num_rows = $query->num_rows();
+
+			return $num_rows;
+		}
+
+		/**
+	     * Retrieve top recent 100 url records. 
+	     * @return array
+	     */
+		public function get_top_url(){
+					  $this->db->order_by('id','DESC');
+			$result = $this->db->get('urls', 0, 100)->result_array();
+			return $result;
+		} 
+
+		/**
+	     * Check records for already existing short 
+	     * @param $short
+	     * @return array or false
+	     */
+		public function check_short_exists($short){
+			
+					 $this->db->where('short', $short);
+			$query = $this->db->get('urls');
+
+			$num_rows = $query->num_rows();
+
+			if($num_rows>0){
+				
+				$result = $query->row_array();
+			
+				return $result;
+
+			} else {
+
+				return false;
+			}
+		}
+		
 	}
-	
-}
+
 /* End of file Url_model.php */
 /* Location: ./application/models/url_model.php */		
